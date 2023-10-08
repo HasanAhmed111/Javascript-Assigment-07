d2c41137d2fd48ce5a66f5a6ce6114ec6f0b4cf6
HasanAhmed111 <144584937+HasanAhmed111@users.noreply.github.com>
2023-10-08 19:13:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Q1
+function element(){
+    alert("Thanks for Visiting");
+}
+element();
+// Q3
+function greet(a) {
+      console.log(a);
+      a.style.backgroundColor = "red";
+      a.style.color = "white";
+    }
+//  CHAPTER 49,50 
+// Q1
+var userInput = prompt("Enter your name");
+console.log(userInput);
+// // Q5
+// var married
+// var maritalStatus = prompt("Please enter your marital status");
+// if(married === false){
+//     return "available" ;
+// } 
+
+
+
+
